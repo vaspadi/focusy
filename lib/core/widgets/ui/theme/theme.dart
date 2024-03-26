15588efc9e42cf924daa16dbf2d1413ed9a22d12
vaspadi <vasiliev.work1@gmail.com>
2024-03-26 08:47:41 +0200

    feat(theme): create light theme with typography

---
+++ lib/core/widgets/ui/theme/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FThemeData {
+  static ThemeData light = ThemeData(
+    textTheme: TextTheme(
+      displayMedium: GoogleFonts.rubik(
+        textStyle: const TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      displaySmall: GoogleFonts.rubik(
+        textStyle: const TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      titleMedium: GoogleFonts.rubik(
+        textStyle: const TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      bodyMedium: GoogleFonts.rubik(
+        textStyle: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      bodySmall: GoogleFonts.rubik(
+        textStyle: const TextStyle(
+          fontSize: 13,
+          color: Color.fromRGBO(30, 30, 30, 1),
+        ),
+      ),
+      labelMedium: GoogleFonts.rubik(
+        textStyle: const TextStyle(
+          fontSize: 17,
+        ),
+      ),
+    ),
+  );
+}
