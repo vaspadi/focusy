e544f361a6a3d1f4bedf9fcb2d5689736ec21a7e
vaspadi <vasiliev.work1@gmail.com>
2024-03-27 10:23:29 +0200

    feat(ui): create square icon button widget

--- lib/core/widgets/index.dart
+++ lib/core/widgets/index.dart
@@ -2,6 +2,7 @@ export './ui/button.dart';
 export './ui/icon_button.dart';
 export './ui/preloader.dart';
 export './ui/scaffold.dart';
+export './ui/square_icon_button.dart';
 export './ui/text.dart';
 export 'app_bar_of_test.dart';
 export 'view_header.dart';
---
+++ lib/core/widgets/ui/square_icon_button.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import 'constants/colors.dart';
+import 'interfaces/button.dart';
+
+class FSquareIconButton extends StatelessWidget implements IButton {
+  final IconData iconData;
+  @override
+  final bool disabled;
+  @override
+  final void Function()? onPressed;
+  final FSquareIconButtonStyle style;
+
+  const FSquareIconButton({
+    required this.iconData,
+    this.disabled = false,
+    this.onPressed,
+    this.style = FSquareIconButtonStyle.normal,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(iconData),
+      onPressed: disabled ? null : onPressed,
+      splashRadius: 1,
+      style: ButtonStyle(
+        iconColor: style.getIconColor(),
+        backgroundColor: style.getBackgroundColor(),
+        side: style.getBorderSize(),
+        shape: MaterialStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3),
+          ),
+        ),
+        elevation: const MaterialStatePropertyAll(1),
+        padding: const MaterialStatePropertyAll(EdgeInsets.zero),
+        minimumSize: const MaterialStatePropertyAll(Size(27, 27)),
+        maximumSize: const MaterialStatePropertyAll(Size(27, 27)),
+        iconSize: const MaterialStatePropertyAll(20),
+      ),
+    );
+  }
+}
+
+enum FSquareIconButtonStyle {
+  normal,
+  selected,
+  success,
+  error;
+
+  const FSquareIconButtonStyle();
+
+  MaterialStateProperty<Color> getBackgroundColor() {
+    final color = () {
+      switch (this) {
+        case FSquareIconButtonStyle.error:
+          return FColors.incorrect;
+        case FSquareIconButtonStyle.success:
+          return FColors.correct;
+        case FSquareIconButtonStyle.normal:
+          return FColors.backTouchable;
+        case FSquareIconButtonStyle.selected:
+          return FColors.text;
+      }
+    }();
+
+    return MaterialStatePropertyAll(color);
+  }
+
+  MaterialStateProperty<BorderSide>? getBorderSize() {
+    switch (this) {
+      case FSquareIconButtonStyle.normal:
+        return const MaterialStatePropertyAll(
+          BorderSide(
+            color: Color.fromRGBO(153, 153, 153, 1),
+            // style: BorderStyle.none,
+          ),
+        );
+      case FSquareIconButtonStyle.selected:
+      case FSquareIconButtonStyle.success:
+      case FSquareIconButtonStyle.error:
+        return null;
+    }
+  }
+
+  MaterialStateProperty<Color> getIconColor() {
+    final color = () {
+      switch (this) {
+        case FSquareIconButtonStyle.normal:
+          return const Color.fromRGBO(153, 153, 153, 1);
+        case FSquareIconButtonStyle.error:
+        case FSquareIconButtonStyle.selected:
+        case FSquareIconButtonStyle.success:
+          return const Color.fromRGBO(255, 255, 255, 1);
+      }
+    }();
+
+    return MaterialStatePropertyAll(color);
+  }
+}
