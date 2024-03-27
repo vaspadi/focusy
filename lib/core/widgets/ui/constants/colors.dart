e08301757b15cfa1aeb54d2068b3c51cdf1a2ae2
vaspadi <vasiliev.work1@gmail.com>
2024-03-27 10:21:28 +0200

    feat(ui): config colors constants

---
+++ lib/core/widgets/ui/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const Color correct = Color.fromRGBO(64, 199, 151, 1);
+  static const Color incorrect = Color.fromRGBO(217, 80, 80, 1);
+
+  static const Color touchable = Color.fromRGBO(219, 219, 219, 1);
+  static const Color backTouchable = Color.fromRGBO(239, 239, 239, 1);
+
+  static const Color text = Color.fromRGBO(30, 30, 30, 1);
+  static const Color back = Color.fromRGBO(255, 255, 255, 1);
+}
